2c9202f0ada81970d8c3aa0e07902c9592757552
Fadhil Yori Hibatullah <fadhil4812@gmail.com>
2022-08-24 12:43:50 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module main
+
+go 1.18
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.4.1
+	github.com/fsnotify/fsnotify v1.5.4
+	github.com/nxadm/tail v1.4.8
+)
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220818161305-2296e01440c6 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"log"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/nxadm/tail"
+)
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	log.Println("MQTT Client Connected.")
+}
+
+var connectionLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	log.Printf("Connection Lost: %s\n", err.Error())
+}
+
+func getMQTTClient(options *mqtt.ClientOptions) mqtt.Client {
+	client := mqtt.NewClient(options)
+	token := client.Connect()
+
+	if token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	return client
+}
+
+func main() {
+	var broker = "tcp://192.168.1.121:1883"
+
+	var default_topic = "mataelang/sensor/v3"
+
+	options := mqtt.NewClientOptions()
+	options.AddBroker(broker)
+	options.SetClientID("mataelang_sensor_snort_v3")
+	options.OnConnect = connectHandler
+	options.OnConnectionLost = connectionLostHandler
+
+	client := getMQTTClient(options)
+
+	topic := default_topic
+
+	filePath := "/home/fadhilyori/Projects/mata-elang/stable/snort3/snort_data/alert_json.txt"
+	// filePath := "/home/fadhilyori/Projects/mata-elang/stable/snort3/test.log"
+
+	messages := make(chan string)
+
+	// Create a tail process
+	t, err := tail.TailFile(
+		filePath, tail.Config{Follow: true})
+	if err != nil {
+		panic(err)
+	}
+
+	// Create routine for sending message from messages channel
+	go func() {
+		var token = nil;
+		for textLine := range messages {
+			log.Printf("Sending snort log... ")
+			token = client.Publish(topic, 0, false, textLine)
+			token.Wait()
+			log.Printf("[ok]\n")
+		}
+	}()
+
+	// Send the message to channel
+	for line := range t.Lines {
+		messages <- line.Text
+	}
+}
